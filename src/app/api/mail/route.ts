b83afc14a485d35d9177a5a0ae79d9bebfce83ef
unknown <antoine.vinsonneau34@gmail.Com>
2024-03-05 11:51:19 +0100

    ok

---
+++ src/app/api/mail/route.ts
@@ -0,0 +1,20 @@
+import KoalaWelcomeEmail from "../../../../emails/koala-welcome";
+import { Resend } from "resend";
+
+const resend = new Resend("re_SodhCFjo_AmCwmnTBaN1DkT9ZzuXkjdvV");
+
+export async function POST(req: Request, res: Response) {
+  const { userFirstname } = await req.json();
+  const { data, error } = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "antoine.vinsonneau34@gmail.com",
+    subject: "Hello World",
+    react: KoalaWelcomeEmail({ userFirstname }),
+  });
+
+  if (error) {
+    return Response.json(error);
+  }
+
+  return Response.json({ message: "Email sent successfully" });
+}
