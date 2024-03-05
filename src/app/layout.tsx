58b06f76ad878d3bcfa72a94da2e0665915ab69a
unknown <antoine.vinsonneau34@gmail.Com>
2024-03-05 11:41:49 +0100

    Supprime warning de merde

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,37 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ThemeProvider, createTheme } from "@mui/material/styles"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import NavBar from '@/components/layout/NavBar';
-import Footer from '@/components/layout/Footer';
-import { darkTheme } from '@/utils/Themes';
-import { Toaster } from 'sonner';
+import NavBar from "@/components/layout/NavBar";
+import Footer from "@/components/layout/Footer";
+import { darkTheme } from "@/utils/Themes";
+import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const meta: Metadata = {
-  title: 'Next.js + MUI',
-  description: 'Next.js + MUI',
-  viewport: 'width=device-width, initial-scale=1.0',
-}
+  title: "Next.js + MUI",
+  description: "Next.js + MUI",
+  viewport: "width=device-width, initial-scale=1.0",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <body className={inter.className}>
-            <NavBar />
-            <Toaster richColors />
-            <div className="md:mr-[5%] md:ml-[5%] mb-[10%]">{children}</div>
-            <Footer />
-
-          </body>
+        <CssBaseline />
+        <body className={inter.className}>
+          <NavBar />
+          <Toaster richColors />
+          <div className="md:mr-[5%] md:ml-[5%] mb-[10%]">{children}</div>
+          <Footer />
+        </body>
       </ThemeProvider>
     </html>
-  )
+  );
 }
