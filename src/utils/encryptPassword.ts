531c7fc918c575397f0f51acdeeddd6735b4c930
unknown <antoine.vinsonneau34@gmail.Com>
2024-03-05 16:30:53 +0100

    ok

--- src/utils/encryptPassword.ts
+++
@@ -1,8 +0,0 @@
-import * as CryptoJS from "crypto-js";
-
-const encryptPassword = (password: string): string => {
-    const passwordCrypt = CryptoJS.AES.encrypt(password, process.env.NEXT_PUBLIC_KEY_PASSWORD).toString()
-    return passwordCrypt
-}
-
-export default encryptPassword
